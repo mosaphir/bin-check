d8b834a66883753dc033cc1d6eb878f600795aac
Mosaphir <skdm0111@gmail.com>
2024-11-09 20:44:35 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 export const metadata = {
   title: 'BIN Checker',
-  description: 'A modern BIN checker tool with enhanced UI',
+  description: 'Generate random test credit card numbers for testing, development,validation and or verification purposes',
 };
 
 export default function RootLayout({
@@ -16,21 +16,21 @@ export default function RootLayout({
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content="BIN Checker, BIN Lookup, Credit Card BIN, Banking Tools, Financial Tools" />
-        <meta name="author" content="Your Name or Company Name" />
+        <meta name="author" content="Nullbite" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourwebsite.com" />
-        <meta property="og:image" content="https://yourwebsite.com/og-image.jpg" />
+        <meta property="og:url" content="https://bin-check-peach.vercel.app/" />
+        <meta property="og:image" content="https://bin-check-peach.vercel.app/f0441c05-5609-4346-8cb8-41e25d4f5965.webp" />
         
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="https://yourwebsite.com/twitter-image.jpg" />
+        <meta name="twitter:image" content="https://bin-check-peach.vercel.app/f0441c05-5609-4346-8cb8-41e25d4f5965.webp" />
         
         <link rel="icon" href="/favicon.ico" />
       </head>
